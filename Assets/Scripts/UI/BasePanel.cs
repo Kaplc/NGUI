64bec032bf9f03e3b89f3c21321ae9ac1ab981f4
Kaplc <755536175@qq.com>
2023-07-15 16:06:34 +0800

    提示面板

---
+++ Assets/Scripts/UI/BasePanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BasePanel<T> : MonoBehaviour where T : class
+{
+    private T instance;
+
+    public T Instance => instance;
+
+    private void Start()
+    {
+        // 自动进行初始化
+        Init();
+    }
+    
+    // 方便子类初始化
+    public abstract void Init();
+
+    private void Awake()
+    {
+        instance = this as T;
+    }
+
+    public virtual void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/TipsPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipsPanel : BasePanel<TipsPanel>
+{
+    public UIButton btnEnter;
+    public UILabel labInfo;
+
+    public override void Init()
+    {
+        btnEnter.onClick.Add(new EventDelegate(() =>
+        {
+            Hide(); // 点击确定隐藏自己
+        }));
+
+        Hide();
+    }
+
+    public void ChangeInfo(string text)
+    {
+        labInfo.text = text;
+    }
+}
